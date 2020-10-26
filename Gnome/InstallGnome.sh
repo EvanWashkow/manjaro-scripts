3fa295123f04adf20c7d1cf2f0528afe24aab856
Evan Washkow <epwashkow@gmail.com>
2020-10-26 15:45:14 -0700

    Fix pacman noconfirm instruction

--- Gnome/InstallGnome.sh
+++ Gnome/InstallGnome.sh
@@ -1,5 +1,5 @@
-sudo pacman -Sy alsa-utils gdm gnome-keyring gnome-terminal noto-fonts
+sudo pacman -S --noconfirm alsa-utils gdm gnome-keyring gnome-terminal noto-fonts
 
-sudo pacman -Sy --asdeps gnome-control-center noto-fonts-cjk noto-fonts-emoji
+sudo pacman -S --noconfirm --asdeps gnome-control-center noto-fonts-cjk noto-fonts-emoji
 
 sudo systemctl enable gdm
