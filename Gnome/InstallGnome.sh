58b63bc3443c9840b7754a4031dfb5654fd51825
Unknown <unknown@example.com>
2020-10-27 16:51:23 -0700

    Add a bunch more dependencies to InstallGnome.sh

--- Gnome/InstallGnome.sh
+++ Gnome/InstallGnome.sh
@@ -2,11 +2,38 @@
 
 sudo pacman -S --noconfirm \
     alsa-utils \
-    gdm gedit \
+    baobab \
+    eog \
+    file-roller \
+    gdm \
+    gedit \
+    gnome-backgrounds \
+    gnome-calculator \
+    gnome-calendar \
+    gnome-contacts \
+    gnome-logs \
     gnome-keyring \
-    gnome-terminal
+    gnome-screenshot \
+    gnome-shell-extensions \
+    gnome-system-monitor \
+    gnome-terminal \
+    gnome-weather \
+    gvfs \
+    nautilus \
+    totem \
+    xdg-user-dirs-gtk
 
 sudo pacman -S --noconfirm --asdeps \
-    gnome-control-center
+    gnome-control-center \
+    gst-plugins-ugly \
+    gst-libav \
+    gvfs-afc \
+    gvfs-goa \
+    gvfs-google \
+    gvfs-gphoto2 \
+    gvfs-mtp \
+    gvfs-nfs \
+    gvfs-smb \
+    unrar
 
 sudo systemctl enable gdm
