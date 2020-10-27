dd4d8b37d8a79d6899429d5415fb69b86c5d47b6
Unknown <unknown@example.com>
2020-10-27 16:23:45 -0700

    Refactor InstallGnome.sh

--- Gnome/InstallGnome.sh
+++ Gnome/InstallGnome.sh
@@ -1,5 +1,12 @@
-sudo pacman -S --noconfirm alsa-utils gdm gedit gnome-keyring gnome-terminal
+//!/bin/sh
 
-sudo pacman -S --noconfirm --asdeps gnome-control-center
+sudo pacman -S --noconfirm \
+    alsa-utils \
+    gdm gedit \
+    gnome-keyring \
+    gnome-terminal
+
+sudo pacman -S --noconfirm --asdeps \
+    gnome-control-center
 
 sudo systemctl enable gdm
