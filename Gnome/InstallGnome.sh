cc616630642431580a9c30676925f939ee88ef8c
Evan Washkow <epwashkow@gmail.com>
2020-10-27 15:00:46 -0700

    Add gedit to Gnome installation script

--- Gnome/InstallGnome.sh
+++ Gnome/InstallGnome.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --noconfirm alsa-utils gdm gnome-keyring gnome-terminal noto-fonts
+sudo pacman -S --noconfirm alsa-utils gdm gedit gnome-keyring gnome-terminal noto-fonts
 
 sudo pacman -S --noconfirm --asdeps gnome-control-center noto-fonts-cjk noto-fonts-emoji
 
