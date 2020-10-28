2d70e9beb9c188e5df1914c93302aecf17435f0c
Unknown <unknown@example.com>
2020-10-28 13:12:04 -0700

    Write script to setup US mirror daily update

---
+++ Pacman/Mirrors/SetupDailyUpdateForUS.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo pacman -S --noconfirm cronie reflector
+sudo systemctl enable cronie
+
+sudo touch /etc/cron.daily/mirrorlist
+sudo sh -c 'echo "#!/bin/sh" > /etc/cron.daily/mirrorlist'
+sudo sh -c 'echo "sudo reflector --country=US --latest=20 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist" >> /etc/cron.daily/mirrorlist'
+sudo chmod +x /etc/cron.daily/mirrorlist
