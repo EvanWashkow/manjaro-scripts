34b9fc4ef63b4b7628eb7ce8dd7cd4a531ba52d4
Unknown <unknown@example.com>
2020-10-28 13:14:46 -0700

    Also refresh mirrors after ranking them

--- Pacman/Mirrors/SetupDailyUpdateForUS.sh
+++ Pacman/Mirrors/SetupDailyUpdateForUS.sh
@@ -6,4 +6,5 @@ sudo systemctl enable cronie
 sudo touch /etc/cron.daily/mirrorlist
 sudo sh -c 'echo "#!/bin/sh" > /etc/cron.daily/mirrorlist'
 sudo sh -c 'echo "sudo reflector --country=US --latest=20 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist" >> /etc/cron.daily/mirrorlist'
+sudo sh -c 'echo "sudo pacman -Sy"  >> /etc/cron.daily/mirrorlist'
 sudo chmod +x /etc/cron.daily/mirrorlist
