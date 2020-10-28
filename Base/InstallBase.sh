deb73e6a11a61c623f716e0d60ce8bb9111c7139
Unknown <unknown@example.com>
2020-10-27 23:25:28 -0700

    Create InstallBase.sh script

---
+++ Base/InstallBase.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo pacman -S --noconfirm base
+
+
+#
+# Clean up: mark all base dependencies as such that may have been explicitly installed before
+#
+
+# https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Getting_the_dependencies_list_of_several_packages
+baseDependencies=$(pacman -Qi base | awk -F'[:<=>]' '/^Depends/ {print $2}')
+sudo pacman -D --asdeps $baseDependencies
+
+# Mark Linux Firmware as being a dependency (of the kernel)
+sudo pacman -D --asdeps linux-firmware
