5a3161bbb423d8192598c6df6290a8c8ed12561a
Unknown <unknown@example.com>
2020-10-28 21:50:25 -0700

    Modify SetupUS.sh to use shared variables

--- Pacman/MirrorRanking/SetupUS.sh
+++ Pacman/MirrorRanking/SetupUS.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# Install components
 sudo pacman -S --noconfirm cronie reflector
 sudo systemctl enable cronie
 
-sudo touch /etc/cron.daily/mirrorlist
-sudo sh -c 'echo "#!/bin/sh" > /etc/cron.daily/mirrorlist'
-sudo sh -c 'echo "sudo reflector --country=US --latest=20 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist" >> /etc/cron.daily/mirrorlist'
-sudo sh -c 'echo "sudo pacman -Sy"  >> /etc/cron.daily/mirrorlist'
-sudo chmod +x /etc/cron.daily/mirrorlist
+# Get shared variables
+source $(dirname $0)/Variables.sh
+
+# Build daily cron to refresh mirrors
+sudo touch $cronFilePath
+sudo sh -c "echo '#!/bin/sh' > $cronFilePath"
+sudo sh -c "echo 'sudo reflector --country=US --latest=20 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist' >> $cronFilePath"
+sudo sh -c "echo 'sudo pacman -Sy'  >> $cronFilePath"
+sudo chmod +x $cronFilePath
