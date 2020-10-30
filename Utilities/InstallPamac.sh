c9c6219734477231d9126f689c47bdd8e132f65c
Evan Washkow <epwashkow@gmail.com>
2020-10-30 14:45:24 -0700

    Move InstallPamac into InstallUtilities

--- Utilities/InstallPamac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S --noconfirm base-devel
-cd ~
-git clone https://aur.archlinux.org/pamac-aur.git
-cd pamac-aur
-makepkg -sic --noconfirm
-cd ..
-rm -rdf pamac-aur
--- Utilities/InstallUtilities.sh
+++ Utilities/InstallUtilities.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# Install git and nano
 sudo pacman -S --noconfirm git nano
+
+# Install Pamac
+sudo pacman -S --noconfirm base-devel
+cd ~
+git clone https://aur.archlinux.org/pamac-aur.git
+cd pamac-aur
+makepkg -sic --noconfirm
+cd ..
+rm -rdf pamac-aur
