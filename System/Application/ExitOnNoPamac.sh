f66e5f6093536f9b14f2f8b784e75d4483b341f1
Evan Washkow <epwashkow@gmail.com>
2020-10-30 22:20:21 -0700

    Remove ExitOnNoPamac

--- System/Application/ExitOnNoPamac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if ! command -v pamac >> /dev/null
-then
-    echo "pamac not installed"
-    exit 1
-fi
--- System/Pacman/Hook/InstallSystemdBootPacmanHook.sh
+++ System/Pacman/Hook/InstallSystemdBootPacmanHook.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-source $(dirname $0)/../../Application/ExitOnNoPamac.sh
-
 pamac build --no-confirm systemd-boot-pacman-hook
 
 echo "Please restart your computer"
