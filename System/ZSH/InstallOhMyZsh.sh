869661aa86ad16743a00ba585f5eab4673f9e6a1
Evan Washkow <epwashkow@gmail.com>
2020-10-30 22:11:42 -0700

    Open nano to edit the default user shell

--- System/ZSH/InstallOhMyZsh.sh
+++ System/ZSH/InstallOhMyZsh.sh
@@ -9,5 +9,8 @@ sudo ln -s /usr/share/oh-my-zsh/zshrc /etc/skel/.zshrc
 rm ~/.zshrc
 cp /etc/skel/.zshrc ~
 
+# Edit the default user shell
+sudo nano /etc/default/useradd
+
 # Output message to restart browser
 echo "Please re-open your terminal"
