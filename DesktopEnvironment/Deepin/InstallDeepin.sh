fe163879f5f7a735dd75d3daabe08d96e3bdb6ae
Evan Washkow <epwashkow@gmail.com>
2020-12-01 23:35:38 -0800

    Include xorg-server in the Deepin installation script

--- DesktopEnvironment/Deepin/InstallDeepin.sh
+++ DesktopEnvironment/Deepin/InstallDeepin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install packages from main repos
-sudo pacman -S deepin deepin-extra
+sudo pacman -S deepin deepin-extra xorg-server
 
 # Enable services
 sudo systemctl enable lightdm
