1914c2f11110923fb8a30182ef6f0d9cddb5ec3f
Evan Washkow <epwashkow@gmail.com>
2020-12-03 14:47:06 -0800

    Create script to install Plasma

---
+++ DesktopEnvironment/Plasma/InstallPlasma.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Install packages from main repos
+sudo pacman -S --noconfirm \
+    kde-network-meta
+    kde-system-meta
+    kde-utilities-meta
+    plasma-meta
+
+# Install optional dependencies from main repos
+sudo pacman -S --noconfirm --asdeps \
+    kde-gtk-config
+    packagekit-qt5
+    pulseaudio-bluetooth
+
+# Enable services
+sudo systemctl enable sddm
