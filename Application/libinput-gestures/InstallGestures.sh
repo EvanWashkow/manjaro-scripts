7366adf57ac00429940c5fb5264b55e536982fd6
Evan Washkow <epwashkow@gmail.com>
2020-12-03 11:57:58 -0800

    Write script to install the gestures gui app for libinput-gestures

---
+++ Application/libinput-gestures/InstallGestures.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# libinput-gestures: https://wiki.archlinux.org/index.php/Libinput#libinput-gestures
+
+# gestures (depends on libinput-gestures): https://aur.archlinux.org/packages/gestures/
+pamac build --no-confirm gestures
+sudo pacman -S --noconfirm --asdeps xdotool
