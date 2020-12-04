b6ee2894acc6837f9ee59a78c1e9bf60b5027bd9
Evan Washkow <epwashkow@gmail.com>
2020-12-04 12:12:02 -0800

    Remove extra spacing

--- DesktopEnvironment/Plasma/InstallPlasma.sh
+++ DesktopEnvironment/Plasma/InstallPlasma.sh
@@ -2,25 +2,17 @@
 
 # Install dependencies from main repos
 sudo pacman -S --noconfirm --asdeps \
-    \
-    # Providers \
-    phonon-qt5-vlc \
-    \
-    # Optional dependencies \
     kde-gtk-config \
     packagekit-qt5 \
+    phonon-qt5-vlc \
     pulseaudio-bluetooth \
     sshfs
 
 # Install packages from main repos
 sudo pacman -S \
-    \
-    # Meta packages \
     kde-system-meta \
     kde-utilities-meta \
     plasma-meta \
-    \
-    # Individual packages \
     kdeconnect
 
 # Enable services
