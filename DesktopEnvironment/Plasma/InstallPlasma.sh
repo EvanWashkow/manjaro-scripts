7c01f4d65b9566dea8d69d6a304734f1faeff9bf
Evan Washkow <epwashkow@gmail.com>
2020-12-03 16:17:03 -0800

    List phonon-qt5-vlc as the provider on phonon-qt5

--- DesktopEnvironment/Plasma/InstallPlasma.sh
+++ DesktopEnvironment/Plasma/InstallPlasma.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
+# Install optional dependencies from main repos
+sudo pacman -S --noconfirm --asdeps \
+    kde-gtk-config \
+    packagekit-qt5 \
+    phonon-qt5-vlc \
+    pulseaudio-bluetooth
+
 # Install packages from main repos
 sudo pacman -S \
     kde-network-meta \
     kde-system-meta \
     kde-utilities-meta \
     plasma-meta
-    
-# @todo: Add phonon-qt5-vlc ?
-
-# Install optional dependencies from main repos
-sudo pacman -S --noconfirm --asdeps \
-    kde-gtk-config \
-    packagekit-qt5 \
-    pulseaudio-bluetooth
 
 # Enable services
 sudo systemctl enable sddm
