ff7622f5f759085681fbe3530808df3aa1c4c2bb
Evan Washkow <epwashkow@gmail.com>
2021-03-05 12:11:55 -0800

    Remove --noconfirm from Desktop's InstallCommon.sh

--- DesktopEnvironment/Common/InstallCommon.sh
+++ DesktopEnvironment/Common/InstallCommon.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Install packages from main repos
-sudo pacman -S --noconfirm \
+sudo pacman -S \
     alsa-utils \
     noto-fonts \
     xdg-user-dirs-gtk \
     xdg-utils
 
 # Install optional dependencies
-sudo pacman -S --noconfirm --asdeps \
+sudo pacman -S --asdeps \
     noto-fonts-cjk \
     noto-fonts-emoji
