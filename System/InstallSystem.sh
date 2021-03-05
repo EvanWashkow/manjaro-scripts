3f4d0d855cbbe641055ae8b5ea0a784784e2e78d
Evan Washkow <epwashkow@gmail.com>
2021-03-05 10:48:22 -0800

    Reorder marking packages as dependencies until after the packages are installed

--- System/InstallSystem.sh
+++ System/InstallSystem.sh
@@ -4,13 +4,6 @@
 # Clean up: mark all base dependencies as such that may have been explicitly installed before
 #
 
-# https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Getting_the_dependencies_list_of_several_packages
-baseDependencies=$(pacman -Qi base | awk -F'[:<=>]' '/^Depends/ {print $2}')
-sudo pacman -D --asdeps $baseDependencies
-
-# Mark Linux Firmware as being a dependency (of the kernel)
-sudo pacman -D --asdeps linux-firmware
-
 
 # Install common packages
 sudo pacman -S \
@@ -23,6 +16,13 @@ sudo pacman -S \
     git \
     nano \
     ntfs-3g
+
+# https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Getting_the_dependencies_list_of_several_packages
+baseDependencies=$(pacman -Qi base | awk -F'[:<=>]' '/^Depends/ {print $2}')
+sudo pacman -D --asdeps $baseDependencies
+
+# Mark Linux Firmware as being a dependency (of the kernel)
+sudo pacman -D --asdeps linux-firmware
 
 # Enable system services
 sudo systemctl enable bluetooth.service
