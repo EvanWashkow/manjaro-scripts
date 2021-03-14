f06964904c4bc7d03d1cfedbbd6774addcf16cd1
Shaina Washkow <shaina@localhost.localdomain>
2021-03-14 14:37:58 -0700

    Move pulseaudio-bluetooth to system install script

--- System/InstallSystem.sh
+++ System/InstallSystem.sh
@@ -17,7 +17,8 @@ sudo pacman -S \
     nano \
     networkmanager \
     ntfs-3g \
-    pamac-gtk
+    pamac-gtk \
+    pulseaudio-bluetooth
 
 # https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Getting_the_dependencies_list_of_several_packages
 baseDependencies=$(pacman -Qi base | awk -F'[:<=>]' '/^Depends/ {print $2}')
