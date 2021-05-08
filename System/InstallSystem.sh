33543e4450b122dbd72ba766006947741c4feb2c
Evan Washkow <epwashkow@gmail.com>
2021-05-08 12:31:30 -0700

    fix: enable pipewire services in system install script

--- System/InstallSystem.sh
+++ System/InstallSystem.sh
@@ -34,3 +34,5 @@ sudo systemctl enable bluetooth.service
 sudo systemctl enable cronie.service
 sudo systemctl enable dhcpcd.service
 sudo systemctl enable NetworkManager.service
+sudo systemctl --user enable pipewire.service
+sudo systemctl --user enable pipewire-media-session.service
